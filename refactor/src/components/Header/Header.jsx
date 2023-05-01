0c3b3fbe163733354802bec64ee6af533d2e8382
Rahul Joshi <rjoshi9000@gmail.com>
2023-05-01 15:04:17 -0700

    Turn logo into link to home page

--- refactor/src/components/Header/Header.jsx
+++ refactor/src/components/Header/Header.jsx
@@ -3,12 +3,12 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <div className="header-logo">
+      <NavLink className="header-logo" to="/">
         <img
           src="https://www.rahulpjoshi.com/img/LOGO.svg"
           alt="Rahul Joshi logo"
         />
-      </div>
+      </NavLink>
       <nav>
         <NavLink exact activeClassName="orange" to="/">
           Home
